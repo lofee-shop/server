f286b2d89d415f56ff2927dd402e983bf863f3e2
aa04260 <aa04260@gmail.com>
2025-02-24 11:03:01 +0900

    Service 단 .java 생성

---
+++ src/main/java/com/example/server/service/CartService.java
@@ -0,0 +1,11 @@
+package com.example.server.service;
+
+import com.example.server.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+    @Autowired
+    private CartRepository cartRepository;
+}
