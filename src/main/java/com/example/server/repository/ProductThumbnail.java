4b1bd5f7fa8fd2a61e5203b2daaa38e67a5ea2ea
aa04260 <aa04260@gmail.com>
2025-02-24 11:02:39 +0900

    Repository 생성

---
+++ src/main/java/com/example/server/repository/ProductThumbnail.java
@@ -0,0 +1,6 @@
+package com.example.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductThumbnail extends JpaRepository<ProductThumbnail, Long> {
+}
