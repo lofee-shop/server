789c839e2b4cb9fc040926a6db9f351ecb67e58d
aa04260 <aa04260@gmail.com>
2025-03-05 14:47:40 +0900

    refreshToken 쿠키 저장,전달로 수정

--- src/main/java/com/example/server/controller/AuthController.java
+++ src/main/java/com/example/server/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,8 +52,21 @@ public class AuthController {
 
 	@Operation(summary = "서명검증 및 회원가입/로그인 API")
 	@PostMapping("verify")
-	public AuthResponse verify(@RequestBody SignatureRequest signatureRequest) {
-		return authService.verifySignature(signatureRequest.getMessage(), signatureRequest.getSignature());
+	public ResponseEntity<AuthResponse> verify(@RequestBody SignatureRequest signatureRequest) {
+		AuthResponse authResponse = authService.verifySignature(signatureRequest.getMessage(),
+			signatureRequest.getSignature());
+
+		ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", authResponse.getRefreshToken())
+			.httpOnly(true)
+			.secure(true) //로컬 개발시 false
+			.path("/")
+			.maxAge(14 * 24 * 60 * 60) //14일 유지
+			.build();
+
+		return ResponseEntity.ok()
+			.header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
+			.body(authResponse);
+
 	}
 
 	@Operation(summary = "로그아웃 API")
@@ -62,8 +78,13 @@ public class AuthController {
 
 	@Operation(summary = "accessToken 만료 시 refreshToken으로 재발급")
 	@PostMapping("/refresh")
-	public ResponseEntity<AuthResponse> refreshAccessToken(@RequestBody String refreshToken) {
+	public ResponseEntity<AuthResponse> refreshAccessToken(@CookieValue(value = "refreshToken") String refreshToken) {
+		if (refreshToken == null || refreshToken.isEmpty()) {
+			return ResponseEntity.status(401).build();
+		}
+
 		try {
+
 			//토큰에서 userid 검출
 			Claims claims = jwtUtil.extractClaims(refreshToken);
 			Long userId = Long.valueOf(claims.getSubject());
