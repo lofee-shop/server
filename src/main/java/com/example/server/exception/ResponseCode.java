bd0a376f74fe4f1d41ef7cd00e1cf4f6541a6097
aa04260 <aa04260@gmail.com>
2025-03-16 13:59:17 +0900

    [feature] - 예외 처리 추가

---
+++ src/main/java/com/example/server/exception/ResponseCode.java
@@ -0,0 +1,23 @@
+package com.example.server.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ResponseCode {
+	// 사용자 관련 오류
+	USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
+
+	// 주소 관련 오류
+	ADDRESS_NOT_FOUND(404, "배송지를 찾을 수 없습니다."),
+
+	// 파일 업로드 오류
+	FILE_UPLOAD_FAILED(400, "파일 업로드에 실패하였습니다.");
+
+	private final int status;
+	private final String message;
+
+	ResponseCode(int status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+}
