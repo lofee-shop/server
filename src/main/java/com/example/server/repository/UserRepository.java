38c0fbffda05fb65d5b62f9f55f4fd4582d72024
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2025-03-23 00:40:37 +0900

    [feat] - 사용자 검색 기능 구현 코드추가

--- src/main/java/com/example/server/repository/UserRepository.java
+++ src/main/java/com/example/server/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.example.server.repository;
 
-import com.example.server.entity.User;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.server.entity.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	List<User> findTop5ByNicknameContainingIgnoreCase(String nickname);
 }
