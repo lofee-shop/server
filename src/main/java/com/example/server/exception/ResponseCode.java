163664fe2a5b674132ff36c539f89c885eabbda4
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2025-03-23 01:07:25 +0900

    [refactor] - 상품(장바구니, 북마크) 관련 오류코드 추가

--- src/main/java/com/example/server/exception/ResponseCode.java
+++ src/main/java/com/example/server/exception/ResponseCode.java
@@ -7,6 +7,15 @@ public enum ResponseCode {
 	// 사용자 관련 오류
 	USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
 
+	// 상품 (장바구니) 관련 오류
+	PRODUCT_NOT_FOUND(404, "상품을 찾을 수 없습니다."),
+	INVALID_REQUEST(400, "요청이 올바르지 않습니다."),
+	FORBIDDEN(403, "로그인이 필요합니다."),
+	STOCK_NOT_ENOUGH(409, "재고가 부족합니다."),
+
+	// 상품 (북마크) 관련 오류
+	ALREADY_BOOKMARKED(409, "이미 북마크한 상품입니다."),
+
 	// 주소 관련 오류
 	ADDRESS_NOT_FOUND(404, "배송지를 찾을 수 없습니다."),
 
