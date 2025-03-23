b26c344e30fe710e27ce1bc1eb43a898aff40305
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2025-03-23 18:23:15 +0900

    [refactor] - 상품 상세페이지 반환값 수정

--- src/main/java/com/example/server/dto/response/ProductDetailResponse.java
+++ src/main/java/com/example/server/dto/response/ProductDetailResponse.java
@@ -1,16 +1,14 @@
 package com.example.server.dto.response;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public record ProductDetailResponse(
 	Long productId,
 	String productName,
-	BigDecimal price,
-	String description,
-	String sellerNickname,
-	String sellerWalletAddress,
-	List<String> images,       // 썸네일 URL 목록
-	Boolean isBookmarked       // 선택. 로그인 사용자 있을 시 북마크 여부
+	Double price,
+	String Info,
+	String sellerName,
+	List<String> imageUrls,
+	boolean isBookmarked
 ) {
 }
