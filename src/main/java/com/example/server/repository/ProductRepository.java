a680d8a3c6e6d7a4a222a4b4573af6b812194062
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2025-03-23 17:35:34 +0900

    [feat] - 상품 Repository 구현

--- src/main/java/com/example/server/repository/ProductRepository.java
+++ src/main/java/com/example/server/repository/ProductRepository.java
@@ -1,12 +1,8 @@
 package com.example.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.server.entity.Product;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	// 필요 시 기본적인 CRUD 외에도 추가 메서드 정의 가능
-	// 예) Optional<Product> findByName(String name);
 }
