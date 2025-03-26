b66a213e47c4a9e20aa5bcbe19d1cf3d34129d25
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2025-03-27 01:37:11 +0900

    Delete src/main/java/com/example/server/dto/response/ProductSearchResponse.java

--- src/main/java/com/example/server/dto/response/ProductSearchResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.dto.response;
-
-import java.util.List;
-
-public record ProductSearchResponse(List<Suggestion> suggestions) {
-
-	public record Suggestion(String type, Long id, String name) {
-	}
-}
