2e3c4106097da4975d13269c8085c8a4dffc5fc9
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2025-03-27 01:40:09 +0900

    Delete src/main/java/com/example/server/repository directory

--- src/main/java/com/example/server/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.server.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
--- src/main/java/com/example/server/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.server.repository;
-
-import com.example.server.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
