b53fc41000738dd1bf9266469e063c88e53d7cbc
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2025-03-27 02:28:22 +0900

    [refactor] - 동일중복 코드 제거

--- src/main/java/com/example/server/exception/ResponseCode.java
+++ src/main/java/com/example/server/exception/ResponseCode.java
@@ -19,15 +19,9 @@ public enum ResponseCode {
 	ADDRESS_NOT_FOUND(404, "배송지를 찾을 수 없습니다."),
 
 	// 파일 업로드 오류
-	FILE_UPLOAD_FAILED(400, "파일 업로드에 실패하였습니다.");
-
-	// 주소 관련 오류
-	ADDRESS_NOT_FOUND(404, "배송지를 찾을 수 없습니다."),
+	FILE_UPLOAD_FAILED(400, "파일 업로드에 실패하였습니다."),
 
 	ADDRESS_LIMIT_EXCEEDED(400, "배송지는 최대 5개까지만 등록할 수 있습니다."),
-
-	// 파일 업로드 오류
-	FILE_UPLOAD_FAILED(400, "파일 업로드에 실패하였습니다."),
 
 	// 이미지 타입 오류
 	INVALID_IMAGE_TYPE(400, "잘못된 이미지 타입입니다."),
